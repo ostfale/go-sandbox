ac5a1b4832d60a96f6d49aefcfa923c8ad2383ab
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2025-10-16 18:27:54 +0200

    Initial Commit

---
+++ SlicesExamples.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func RunSlicesExamples() {
+	fmt.Println("\nSlices Examples")
+	// empty slices
+	sep("Empty Slice")
+	var xi []int
+	fmt.Printf("Is an empty slice nil: %t", xi == nil)
+
+	sep("Compare Slices")
+	// compare slices
+	x := []int{1, 2, 3, 4, 5}
+	y := []int{1, 2, 3, 4, 5}
+	z := []int{1, 2, 3, 4, 5, 6}
+	s := []string{"a", "b", "c", "d", "e"}
+
+	fmt.Printf("\nCompare equal slices: %t\n", slices.Equal(x, y)) // true
+	fmt.Printf("Compare unequal slices: %t\n", slices.Equal(x, z)) // false
+	fmt.Println(slices.Equal(x, z))                                // true
+	fmt.Printf("Print string slice: %s", s)
+	//	fmt.Println(slices.Equal(x,s))        // does not compile
+
+	sep("Append Slice")
+	// append function
+	sApp := []int{10, 20}
+	fmt.Printf("\nInitial slice: %v, Length: %d, Capacity: %d\n", sApp, len(sApp), cap(sApp))
+	sApp = append(sApp, 30)
+	fmt.Printf("After append:  %v, Length: %d, Capacity: %d\n", sApp, len(sApp), cap(sApp))
+
+	yApp := []int{21, 31, 41}
+	sApp = append(sApp, yApp...)
+	fmt.Printf("After append another slice:  %v, Length: %d, Capacity: %d\n", sApp, len(sApp), cap(sApp))
+
+	sep("Empty Slice")
+	// empty a slice
+	sClear := []string{"first", "second"}
+	fmt.Println(sClear, len(sClear)) // [first,second] 3
+	clear(sClear)
+	fmt.Println(sClear, len(sClear)) // [ ] 3
+
+	sep("Make Slice")
+	// make examples
+	sMake := make([]int, 3)
+	sMake1 := make([]int, 3, 5)
+	fmt.Println(len(sMake), cap(sMake), len(sMake1), cap(sMake1))
+}
+
+func sep(title ...string) {
+	fmt.Printf("\n\n-------------------- %s --------------------\n", title)
+}
