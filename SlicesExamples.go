67288d093193d611020d597fc6bd5dc2131752ac
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2025-10-17 19:22:05 +0200

    Add examples for slicing, copying, and converting arrays to slices in Go.

--- SlicesExamples.go
+++ SlicesExamples.go
@@ -48,6 +48,51 @@ func RunSlicesExamples() {
 	sMake := make([]int, 3)
 	sMake1 := make([]int, 3, 5)
 	fmt.Println(len(sMake), cap(sMake), len(sMake1), cap(sMake1))
+
+	sep("Slicing Slices")
+	xSlice := []string{"a", "b", "c", "d", "e"}
+	ySlice := xSlice[:2]
+	zSlice := xSlice[1:]
+	dSlice := xSlice[1:3]
+	eSlice := xSlice[:]
+	fmt.Println("xSlice", xSlice)
+	fmt.Println("ySlice", ySlice)
+	fmt.Println("zSlice", zSlice)
+	fmt.Println("dSlice", dSlice)
+	fmt.Println("eSlice", eSlice)
+
+	sep("Sharing memory")
+	// changing slices by using the same memory
+	xSlice[0] = "A"
+	fmt.Println("xSlice", xSlice)
+	fmt.Println("ySlice", ySlice)
+
+	sep("Copying Slices")
+	// copy slices
+	xCopy := []int{1, 2, 3, 4, 5}
+	yCopy := make([]int, 4)
+	num := copy(yCopy, xCopy) // first argument is destination slice, second is source slice
+	fmt.Println(yCopy, num)
+
+	// copy slices from an array
+	xCopyArray := []int{1, 2, 3, 4}
+	dArray := [4]int{5, 6, 7, 8}
+	yCopyArray := make([]int, 2)
+	copy(yCopyArray, dArray[:])
+	fmt.Println(yCopyArray)
+	copy(dArray[:], xCopyArray)
+	fmt.Println(dArray)
+
+	sep("Converting Array to Slice")
+	// convert array to slice
+	x1Slice := []int{1, 2, 3, 4}
+	x1Array := [4]int(x1Slice)
+	smallArray := [2]int(x1Slice)
+	x1Slice[0] = 10
+	fmt.Println(x1Slice)
+	fmt.Println(x1Array)
+	fmt.Println(smallArray)
+
 }
 
 func sep(title ...string) {
---
+++ StringExample.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func RunStringExample() {
+	fmt.Println("String Example")
+}
